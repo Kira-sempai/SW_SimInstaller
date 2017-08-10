52b02aa83269b53430219fe63c7e514f596b9081
Andrey Vinogradov <andrey.vinogradov@teplomonitor.ru>
2017-08-10 15:07:48 +0300

    createmsi.bat: fix ru plugin path

--- createmsi.bat
+++ createmsi.bat
@@ -12,4 +12,4 @@ set PATH=%WIXDIR%;%PATH%
 @echo on
 
 candle product.wxs features.wxs files.wxs shortcuts.wxs WixUI_custom.wxs > candle.log
-light -ext WixUIExtension -cultures:ru-ru -loc %WIXDIR%/WixUI_ru-ru.v3/WixUI_ru-ru.wxl -out msi\SmartWebInstall.msi product.wixobj features.wixobj files.wixobj shortcuts.wixobj WixUI_custom.wixobj > light.log
+light -ext WixUIExtension -cultures:ru-ru -loc "%WIXDIR%\WixUI_ru-ru.v3\WixUI_ru-ru.wxl" -out msi\SmartWebInstall.msi product.wixobj features.wixobj files.wixobj shortcuts.wixobj WixUI_custom.wixobj > light.log
